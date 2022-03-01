4b4f7c41548f4cb0d9233392b574b1c437f9faf1
Chansup Byun <cbyun@ll.mit.edu>
2022-03-01 15:55:06 -0500

    Add gridPython_Ver() function

---
+++ src/gridPython_Ver.py
@@ -0,0 +1,21 @@
+import pathlib
+
+def gridPython_Ver():
+    """gridPython_Ver - Return the current gridPython version.
+    
+    Usage:
+    version = gridPython_Ver()
+    
+    location: current location of the gridPython library (dtype: string)
+    version:  gridPython version (dtype: string)
+    
+    """
+    
+    location = pathlib.Path().absolute()
+    version = '0.1.0'
+    
+    print('gridPython version: %s'%(version))
+    print('          Location: %s'%(location))
+    
+    return version
+
