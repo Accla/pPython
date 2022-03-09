0dd02b207876e46b117ccb339aaa3ec9c6a1f2cf
Chansup Byun <cbyun@ll.mit.edu>
2022-03-09 08:20:04 -0500

    Initial check in for PITFALLS/FALLS implementation

---
+++ dmat/src/gen_falls.py
@@ -0,0 +1,113 @@
+class Falls:
+    """Define empty FALLS class.
+    """
+    pass
+
+# Modified implemention to distribute elements across processors as evenly as possible.
+#
+def gen_falls(pf, pid):
+    """Generates the local FALLS (FAmiLy of Line Segmentts) structure for processor, pid, from the given
+    instance of PitFalls class.
+
+    Usage:
+    ------
+    f = gen_falls(pf, pid)
+
+    pf: insttance of the PitFalls class
+    pid: process rank, starting from zero
+
+    The following are the fields of each falls F:
+    L: starting index of the first block
+    R: ending index of the first block
+    S: stride between successive L's
+    N: number of equally spaced, equally sized blocks of elements
+    LOCAL_LEN: local length of the data on the local processor
+        NOTE: If dimensions are evenly divisible, LOCAL_LEN is
+                always (R-L+1)*N
+    COMPLETE_CYCLE: flag that indicates whether the processor has
+                    incomplete cycles
+    COMPLETE_BLOCK: flag that indicates whether the processor has
+                    incomplete blocks
+
+    Original Author: Nadya Travinin (pMatlab)
+    Author: Dr. Chansup Byun (gridPython)
+
+    References: Shankar Ramaswamy and Prithviraj Banerjee. Automatic Generation of Efficient Array
+                Redistribution Routines for Distributed Memory Multicomputers. IEEE 1995.
+    """
+
+    DEBUG = 0
+    if DEBUG:
+        print('--> Entering gen_falls')
+
+    # Create an empty Falls class.
+    f = Falls()
+
+    f.l = pf.l+pid*pf.d
+    f.r = pf.r+pid*pf.d
+    f.s = pf.s
+    f.n = pf.n
+    # additional info used to calculate global index in get_global_ind()
+    f.dist = pf.dist
+
+    # compute local length
+    block_size = f.r-f.l+1 # block size
+    if pf.rem_cycle == 0:
+        f.local_len = f.n*block_size
+        f.complete_cycle = True  # flag that signifies that the local data has no incomplete cycles
+        f.complete_block = True  # flag that signifies that the local data has no incomplete blocks
+        # (i.e. local indices can be computed directly from the falls info without
+        # dealing with local length)
+    else: # rem_cycle ~= 0
+        # number of blocks in the incomplete cycle (both complete
+        # and incomplete)
+        num_blocks = math.ceil(pf.rem_cycle/block_size)
+        # the length of incomplete block (if one exists)
+        rem_block = pf.rem_cycle%block_size
+        if DEBUG:
+            print('num_blocks: %d'%(num_blocks))
+            print('rem_block: %d'%(rem_block))
+
+        if f.dist == 'b':
+            if (pid)<pf.rem_cycle:
+                # local processor does not have an incomplete cycle
+                # and has no incomplete blocks
+                f.local_len = (f.n)*block_size
+                f.complete_cycle = True
+                f.complete_block = True
+            else:
+                # local processor has an incomplete cycle with an
+                # incomplete block
+                f.local_len = (f.n)*block_size-1
+                f.complete_cycle = False
+                f.complete_block = False
+                # adjustment for incomplete cycle & incomple block
+                f.l = pf.l+pid*pf.d-(pid-pf.rem_cycle)
+                f.r = pf.r+pid*pf.d-(pid-pf.rem_cycle)-1
+        else:
+            if (pid+1)<num_blocks:
+                # local processor does not have an incomplete cycle
+                # and has no incomplete blocks
+                f.local_len = (f.n)*block_size
+                f.complete_cycle = True
+                f.complete_block = True
+            elif (pid+1)>num_blocks:
+                # local processor has one less block (incomplete cycle)
+                # but no imcomplete blocks
+                f.local_len = (f.n-1)*block_size;
+                f.complete_cycle = False
+                f.complete_block = True
+            elif (pid+1)==num_blocks:
+                if rem_block==0:
+                    # local processor has no incomplete cycles or blocks
+                    f.local_len = f.n*block_size
+                    f.complete_cycle = True
+                    f.complete_block = True
+                else:
+                    # local processor has an incomplete cycle with an
+                    # incomplete block
+                    f.local_len = (f.n-1)*block_size+rem_block
+                    f.complete_cycle = False
+                    f.complete_block = False
+    return f
+
---
+++ dmat/src/gen_pitfalls.py
@@ -0,0 +1,125 @@
+class PitFalls:
+    """Define empty PITFALLS class.
+    """
+    pass
+
+import math
+
+def gen_pitfalls(np, dist_spec, dim_len, overlap=None):
+    """Given the number of processors, distribution spec, and the length of the dimension,
+    generates all the PITFALLS (Process Index Tagged FALLS) information.
+
+    Usage:
+    ------
+    p = gen_pitfalls(np, dist_spec, dim_len[, overlap])
+
+    np: number of processors along which the dimension is distributed
+    dist_spec: distribution specification with two possible keys.
+        The dist key is mandatory and could have the following values:
+        'b' - block
+        'c' - cyclic
+        'bc' - block-cyclic
+        if dist_spec['dist'] == 'bc', the block size 'b_size' must
+        also be defined.
+     dim_len: length of the dimension for which the PITFALLS is being
+        created.
+
+    Returns p:
+    P is a single PITFALLS used to describe the distribution. P will
+    have an extra field (p.rem_cycle) which will store the length of
+    the incomplete cycle in the current distribution. This way 3
+    PITFALLS will not be necessary to represent non-evenly divisible
+    dimensions.
+    P has the following fields:
+        p.L: starting index of the first block on the first processor
+        p.R: ending index of the first block on the first processor
+        p.S: stride between successive L's
+        p.N: number of equally spaced, equally sized blocks of elements
+                per processor
+        p.D: spacing between L's of successive processor FALLS
+        p.P: number of processors
+        p.REM_CYCLE: the length of the incomplete cycle.
+            IMPORTANT: p.N includes the incomplete cycle if one exists, i.e. the
+                PITFALLS calculation rounds up the length of the dimension and
+                computes the PITFALLS information as if the dimension was
+                evenly divisible by the cycle length.
+        p.dist: distribution spec ('b', 'c', or 'bc'  )
+
+    Original Author: Nadya Travinin (pMatlab)
+    Author: Dr. Chansup Byun (gridPython)
+
+    References: Shankar Ramaswamy and Prithviraj Banerjee. Automatic Generation of Efficient Array
+        Redistribution Routines for Distributed Memory Multicomputers. IEEE 1995.
+
+    """
+
+    # Create an empty PitFalls class.
+    p = PitFalls()
+
+    if not overlap:  # no overlap
+        # store block size
+        if dist_spec['dist'] == 'b':
+            # ToDo: raise by 1 (ceil, pMatlab implementation) or not?
+            b_size = math.ceil(dim_len/np)
+
+        elif dist_spec['dist'] == 'c':
+            b_size = 1
+        elif dist_spec['dist'] == 'bc':
+            b_size = dist_spec['b_size']
+
+        # cycle length
+        cycle_len = b_size*np
+        # number of cycles - both complete and incomplete
+        # ToDo: raise by 1 or not?
+        num_cycles = math.ceil(dim_len/cycle_len)
+        # length of incomplete cycle
+        rem_cycle = dim_len%np
+        # additional info used when calculating global index in get_global_ind()
+        p.dist = dist_spec['dist']
+
+        # create the PITFALLS data structure
+        p.l = 1
+        p.r = b_size
+
+
+        # !!!THIS PRODUCES STRIDE>1 EVEN IF NUM_CYCLES==1...MAKE SURE THIS WORKS
+        # !!!This might influence the intersection algorithm
+        p.s = b_size*np
+
+        p.n = num_cycles
+        p.d = b_size
+        p.p = np
+        p.rem_cycle = rem_cycle
+
+    else:  # overlap  (Need to verify, not yet tested)
+        # store block size
+        if dist_spec['dist'] == 'b':
+            # ToDo: raise by 1 (ceil, pMatlab implementation) or not?
+            b_size = math.ceil(dim_len/np)
+        else:
+            print('ERROR(gen_pitfalls): Overlap is only supported for block distributions.')
+
+        # cycle length
+        cycle_len = b_size*np+overlap
+        # number of cycles - both complete and incomplete
+
+        # ToDo: raise by 1 or not?
+        num_cycles = math.ceil(dim_len/cycle_len)
+        # length of incomplete cycle
+        rem_cycle = dim_len%cycle_len
+
+        # create the PITFALLS data structure
+        p.l = 1
+        p.r = b_size+overlap
+
+        # !!!THIS PRODUCES STRIDE>1 EVEN IF NUM_CYCLES==1...MAKE SURE THIS WORKS
+        # !!!This might influence the intersection algorithm
+        p.s = b_size*np
+
+        p.n = num_cycles
+        p.d = b_size
+        p.p = np
+        p.rem_cycle = rem_cycle
+
+    return p
+
