0e74693ed25f79042a83b205ab86b280394de8de
Chansup Byun <cbyun@ll.mit.edu>
2022-04-11 08:17:56 -0400

    Add the addition and substraction operations with GridDmat class

--- dmat/src/GridDmat.py
+++ dmat/src/GridDmat.py
@@ -1,5 +1,40 @@
+from local import *
+from put_local import *
+
 class GridDmat:
-    """Define empty GridDmat class.
+    """Define GridDmat class.
     """
-    pass
+
+    def __init__(self):
+        pass
+
+    def __add__(self, other):
+        """Implement addition of Dmat() + Float
+        """
+        if isinstance(other,(float)):
+            # Extract local portion of a distributed array
+            d_local = local(self)
+            # update local array
+            d_local = d_local + other
+            # update the distributed array
+            self = put_local(self,d_local)
+        else:
+            print('The type, %s, is not supported for the add operator with GridDmat class yet.'%(type(other)))
+            exit()
+        return self
+
+    def __sub__(self, other):
+        """Implement subtraction of Dmat() - Float
+        """
+        if isinstance(other,(float)):
+            # Extract local portion of a distributed array
+            d_local = local(self)
+            # update local array
+            d_local = d_local - other
+            # update the distributed array
+            self = put_local(self,d_local)
+        else:
+            print('The type, %s, is not supported for the subtraction operator with GridDmat class yet.'%(type(other)))
+            exit()
+        return self
 
