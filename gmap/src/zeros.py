a916e1abeb9e54d8d8067786cdbe2f059af89b4c
Chansup Byun <cbyun@ll.mit.edu>
2022-04-11 10:36:51 -0400

    Fix to handle more general function signature with zeros

--- gmap/src/zeros.py
+++ gmap/src/zeros.py
@@ -4,142 +4,190 @@ import numpy as np
 from GridMap import *
 from grid_zeros import *
 
-@dispatch(int,type(GridMap()))
-def zeros(m,map):
-    """grid_zeros() wrapper method.
-    """
-    n=None
-    q=None
-    r=None
-    dtype = None
-    # print('Called zeros(m,map) where m is a scalar')
-    return grid_zeros(m,n,q,r,map,dtype)
-
-@dispatch(np.ndarray,type(GridMap()))
+@dispatch(object,type(GridMap()))
 def zeros(v,map):
     """grid_zeros() wrapper method.
     """
     
-    # Check if len(v) is equal to map.dim
-    if not (len(v) == map.dim):
-        print('ERROR(zeros): the dimensions of the given list and the map does not match.')
-        exit()
+    if isinstance(v,(np.ndarray)):
+        # Check if len(v) is equal to map.dim
+        if not (len(v) == map.dim):
+            print('ERROR(zeros): the dimensions of the given list and the map does not match.')
+            exit()
         
-    n=None
-    q=None
-    r=None
+        n=None
+        q=None
+        r=None
 
-    # Extract elements from the list.
-    m = v[0]
-    if len(v) == 2:
-        n = v[1]
-    elif len(v) == 3:
-        n = v[1]
-        q = v[2]
-    elif len(v) == 4:
-        n = v[1]
-        q = v[2]
-        r = v[3]
+        # Extract elements from the list.
+        m = v[0]
+        if len(v) == 2:
+            n = v[1]
+        elif len(v) == 3:
+            n = v[1]
+            q = v[2]
+        elif len(v) == 4:
+            n = v[1]
+            q = v[2]
+            r = v[3]
+        else:
+            print('ERROR(zeros): matrix dimension should be less than or equal to 4.')
+            exit()
+    elif isinstance(v,(int,np.int64)):
+        m=v
+        n=None
+        q=None
+        r=None
     else:
-        print('ERROR(zeros): matrix dimension should be less than or equal to 4.')
+        print('ERROR(zeros): data type, %s, is not supported for matrix size.'%(type(v)))
         exit()
-        
     dtype = None
     # print('Called zeros(m,map) where m is a np.ndarray (vector)')
     return grid_zeros(m,n,q,r,map,dtype)
 
-@dispatch(int,int,type(GridMap()))
+@dispatch(object,object,type(GridMap()))
 def zeros(m,n,map):
     """grid_zeros() wrapper method.
     """
+    if not isinstance(m,(int,np.int64)):
+        print('ERROR(zeros): data type, %s, is not supported for matrix size.'%(type(v)))
+        exit()
+    if not isinstance(n,(int,np.int64)):
+        print('ERROR(zeros): data type, %s, is not supported for matrix size.'%(type(v)))
+        exit()
     q=None
     r=None
     dtype = None
     # print('Called zeros(m,n,map)')
     return grid_zeros(m,n,q,r,map,dtype)
 
-@dispatch(int,int,int,type(GridMap()))
+@dispatch(object,object,object,type(GridMap()))
 def zeros(m,n,q,map):
     """grid_zeros() wrapper method.
     """
+    if not isinstance(m,(int,np.int64)):
+        print('ERROR(zeros): data type, %s, is not supported for matrix size.'%(type(v)))
+        exit()
+    if not isinstance(n,(int,np.int64)):
+        print('ERROR(zeros): data type, %s, is not supported for matrix size.'%(type(v)))
+        exit()
+    if not isinstance(q,(int,np.int64)):
+        print('ERROR(zeros): data type, %s, is not supported for matrix size.'%(type(v)))
+        exit()
     r=None
     dtype = None
     # print('Called zeros(m,n,q,map)')
     return grid_zeros(m,n,q,r,map,dtype)
 
-@dispatch(int,int,int,int,type(GridMap()))
+@dispatch(object,object,object,object,type(GridMap()))
 def zeros(m,n,q,r,map):
     """grid_zeros() wrapper method.
     """
+    if not isinstance(m,(int,np.int64)):
+        print('ERROR(zeros): data type, %s, is not supported for matrix size.'%(type(v)))
+        exit()
+    if not isinstance(n,(int,np.int64)):
+        print('ERROR(zeros): data type, %s, is not supported for matrix size.'%(type(v)))
+        exit()
+    if not isinstance(q,(int,np.int64)):
+        print('ERROR(zeros): data type, %s, is not supported for matrix size.'%(type(v)))
+        exit()
+    if not isinstance(r,(int,np.int64)):
+        print('ERROR(zeros): data type, %s, is not supported for matrix size.'%(type(v)))
+        exit()
     dtype = None
     # print('Called zeros(m,n,q,rmap)')
     return grid_zeros(m,n,q,r,map,dtype)
 #
 # Use of dtype definition
 #
-@dispatch(int,type(GridMap()),str)
-def zeros(m,map,dtype):
-    """grid_zeros() wrapper method.
-    """
-    n=None
-    q=None
-    r=None
-    # print('Called zeros(m,map) with m as a scalar & dtype')
-    return grid_zeros(m,n,q,r,map,dtype)
-
-@dispatch(np.ndarray,type(GridMap()),str)
+@dispatch(object,type(GridMap()),str)
 def zeros(v,map,dtype):
     """grid_zeros() wrapper method.
     """
     
-    # Check if len(v) is equal to map.dim
-    if not (len(v) == map.dim):
-        print('ERROR(zeros): the dimensions of the given list and the map does not match.')
-        exit()
-        
-    n=None
-    q=None
-    r=None
-
-    # Extract elements from the list.
-    m = v[0]
-    if len(v) == 2:
-        n = v[1]
-    elif len(v) == 3:
-        n = v[1]
-        q = v[2]
-    elif len(v) == 4:
-        n = v[1]
-        q = v[2]
-        r = v[3]
+    if isinstance(v,(np.ndarray)):
+        # Check if len(v) is equal to map.dim
+        if not (len(v) == map.dim):
+            print('ERROR(zeros): the dimensions of the given list and the map does not match.')
+            exit()
+        n=None
+        q=None
+        r=None
+        # Extract elements from the list.
+        m = v[0]
+        if len(v) == 2:
+            n = v[1]
+        elif len(v) == 3:
+            n = v[1]
+            q = v[2]
+        elif len(v) == 4:
+            n = v[1]
+            q = v[2]
+            r = v[3]
+        else:
+            print('ERROR(zeros): matrix dimension should be less than or equal to 4.')
+            exit()
+    elif isinstance(v,(int,np.int64)):
+        m=v
+        n=None
+        q=None
+        r=None
     else:
-        print('ERROR(zeros): matrix dimension should be less than or equal to 4.')
+        print('ERROR(zeros): data type, %s, is not supported for matrix size.'%(type(v)))
         exit()
-        
+
     # print('Called zeros(m,map) where m is a np.ndarray (vector)')
     return grid_zeros(m,n,q,r,map,dtype)
 
-@dispatch(int,int,type(GridMap()),str)
+@dispatch(object,object,type(GridMap()),str)
 def zeros(m,n,map,dtype):
     """grid_zeros() wrapper method.
     """
+    if not isinstance(m,(int,np.int64)):
+        print('ERROR(zeros): data type, %s, is not supported for matrix size.'%(type(v)))
+        exit()
+    if not isinstance(n,(int,np.int64)):
+        print('ERROR(zeros): data type, %s, is not supported for matrix size.'%(type(v)))
+        exit()
     q=None
     r=None
     # print('Called zeros(m,n,map) with dtype')
     return grid_zeros(m,n,q,r,map,dtype)
 
-@dispatch(int,int,int,type(GridMap()),str)
+@dispatch(object,object,object,type(GridMap()),str)
 def zeros(m,n,q,map,dtype):
     """grid_zeros() wrapper method.
     """
+    if not isinstance(m,(int,np.int64)):
+        print('ERROR(zeros): data type, %s, is not supported for matrix size.'%(type(v)))
+        exit()
+    if not isinstance(n,(int,np.int64)):
+        print('ERROR(zeros): data type, %s, is not supported for matrix size.'%(type(v)))
+        exit()
+    if not isinstance(q,(int,np.int64)):
+        print('ERROR(zeros): data type, %s, is not supported for matrix size.'%(type(v)))
+        exit()
     r=None
     # print('Called zeros(m,n,q,map) with dtype')
     return grid_zeros(m,n,q,r,map,dtype)
 
-@dispatch(int,int,int,int,type(GridMap()),str)
+@dispatch(object,object,object,object,type(GridMap()),str)
 def zeros(m,n,q,r,map,dtype):
     """grid_zeros() wrapper method.
     """
+    if not isinstance(m,(int,np.int64)):
+        print('ERROR(zeros): data type, %s, is not supported for matrix size.'%(type(v)))
+        exit()
+    if not isinstance(n,(int,np.int64)):
+        print('ERROR(zeros): data type, %s, is not supported for matrix size.'%(type(v)))
+        exit()
+    if not isinstance(q,(int,np.int64)):
+        print('ERROR(zeros): data type, %s, is not supported for matrix size.'%(type(v)))
+        exit()
+    if not isinstance(r,(int,np.int64)):
+        print('ERROR(zeros): data type, %s, is not supported for matrix size.'%(type(v)))
+        exit()
     # print('Called zeros(m,n,q,r,map) with dtype')
     return grid_zeros(m,n,q,r,map,dtype)
 
