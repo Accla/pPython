cb7d5707168bb0ac1401ba92613db40e66dd0702
Chansup Byun <cbyun@ll.mit.edu>
2022-05-13 08:14:28 -0400

    Fix missing return statement

---
+++ dmat/src/ndims.py
@@ -0,0 +1,21 @@
+from size import *
+
+def ndims(a):
+    """
+    NDIMS Returns the number of dimensions of the DMAT.
+
+    Python version: Dr. Chansup Byun
+    Author: Nadya Travinin
+    """
+
+    n = len(size(a))
+    return n
+
+    """
+    gridPython: Dr. Chansup Byun
+    pMatlab: Parallel Matlab Toolbox
+    Software Engineer: Ms. Nadya Travinin (nt@ll.mit.edu)
+    Architect: Dr. Jeremy Kepner (kepner@ll.mit.edu)
+    MIT Lincoln Laboratory
+    """
+
