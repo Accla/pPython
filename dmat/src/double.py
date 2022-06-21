b3aa5c0e316d15a8d3d38a127a96b46f27f9ee3d
Chansup Byun <cbyun@ll.mit.edu>
2022-06-21 12:46:31 -0400

    Add dmat/double()

---
+++ dmat/src/double.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def double(d):
+    """
+    DOUBLE Convert each local part of the DMAT to double precision.
+    DOUBLE(D) returns the double precision value for the local part of D in
+    a DMAT with the same mapping and dimensions as D.
+    If the local part of D is already a double precision array, DOUBLE 
+    has no effect.
+    
+    Author:   Nadya Travinin
+    Python version: Dr. Chansup Byun (cbyun@ll.mit.edu)
+    """
+    if isinstance(d, GridDmat):
+        d.local = np.float64(d.local)
+    else:
+        print('dmat/double: upsupported data type')
+        exit()
+
+    return d
+
