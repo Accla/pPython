af2b330c2029b44d431d579d74eaec3a662417b1
Chansup Byun <cbyun@ll.mit.edu>
2023-02-22 09:52:39 -0500

    Set pPython version as v0.9.6 in pPython_ver

--- src/pPython_ver.py
+++ src/pPython_ver.py
@@ -13,7 +13,7 @@ def pPython_ver(default_display = None):
     """
     
     location = pathlib.Path().absolute()
-    version = '0.9.5'
+    version = '0.9.6'
     
     if default_display:
         print('pPython version: %s'%(version))
