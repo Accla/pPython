Accla/pPython

commit e60b6fb65cd2f4acba8366dbc6431a1323345bd5

Fix a bug when launching an interactive triples mode job with Np=1 (Nnode=Nppn=1)

src/pypPython/PythonMPI/src/pyMPI_Commands.py
@@ -66,7 +66,7 @@ def pyMPI_Commands(py_file,rank,MPI_COMM_WORLD,**argv):
 
     # Get info on the target machine.
     machine_id = MPI_COMM_WORLD['machine_id'][rank]
-    if (grid_config['interactive']) and (grid_config['nppn'] == 1) and rank > 0:
+    if EPPAC and (grid_config['interactive']) and (grid_config['nppn'] == 1) and rank > 0:
         machine_id = machine_id-1
     machine = MPI_COMM_WORLD['machine_db']['machine'][machine_id]
     remote_launch = MPI_COMM_WORLD['machine_db']['remote_launch'][machine_id]

src/pypPython/grid/check_runtime.py
@@ -74,6 +74,8 @@ def check_runtime( n_proc, machines, grid_config ):
                 grid_job = True
             else:
                 grid_job = False
+    grid_config['interactive'] = interactive
+    grid_config['grid_job'] = grid_job
     
     # Determine if a specific CPU type is requested
     # Old partition also requires partition name 
@@ -136,7 +138,7 @@ def check_runtime( n_proc, machines, grid_config ):
     n_proc_req = requested + interactive
     
     # Create a fictious machine list when 'grid[&]' is used
-    if grid_job:
+    if grid_config['grid_job']:
         machines = []
         # set the Pid=0 machine for an interactive job
         if interactive:
@@ -172,6 +174,9 @@ def check_runtime( n_proc, machines, grid_config ):
                 machines.append('grid_slurm_'+node_strid)
     
     # Convert machines into a dictionary variable if needed
+    if isinstance(machines,str):
+        # Special case: interactive triples mode job with Npo=1
+        machines = {}
     machines,islocal = convert_to_dict(machines,host)
     #
     # Override islocal based on interactive
@@ -182,8 +187,6 @@ def check_runtime( n_proc, machines, grid_config ):
     OS.islocal = islocal
     # save to grid_config['islocal'] which will be saved in MPI_COMM_WORLD
     grid_config['islocal'] = islocal
-    grid_config['grid_job'] = grid_job
-    grid_config['interactive'] = interactive
 
     #
     # Override if PPYTHON_LOCAL_FS is defined
@@ -196,7 +199,7 @@ def check_runtime( n_proc, machines, grid_config ):
     else:
         local_fs = 1
     # Set local_fs = 0 for non grid jobs
-    if not grid_job:
+    if not grid_config['grid_job']:
         local_fs = 0
     grid_config['local_fs'] = local_fs
     # Check compatibality between interactive job versus messaging kernel using local filesystem

src/pypPython/grid/check_triples.py
@@ -51,6 +51,13 @@ def check_triples(cluster_name,cpu_type,n_proc,grid_config):
         else:
             n_proc_req = nnode * nppn
         
+        # Special case: interactive triples mode job with [1,1,NTPP], in order words, n_proc_req = 1
+        if grid_config['interactive'] and n_proc_req == 1:
+            print('interactive triples mode job with NNODE*NPPN=1')
+            grid_config['EPPAC'] = False
+            grid_config['grid_job'] = False
+            return n_proc_req, grid_config
+
         # update grid_config for triples mode job
         grid_config['nnode'] = nnode
         grid_config['ntasks'] = nnode

commit 6aff1ea2e54d22a1e405c1cd688eaf875922fc1e

Fix a bug introduced with e60b6fb65cd2f4acba8366dbc6431a1323345bd5

src/pypPython/PythonMPI/src/pyMPI_Commands.py
@@ -66,8 +66,6 @@ def pyMPI_Commands(py_file,rank,MPI_COMM_WORLD,**argv):
 
     # Get info on the target machine.
     machine_id = MPI_COMM_WORLD['machine_id'][rank]
-    if EPPAC and (grid_config['interactive']) and (grid_config['nppn'] == 1) and rank > 0:
-        machine_id = machine_id-1
     machine = MPI_COMM_WORLD['machine_db']['machine'][machine_id]
     remote_launch = MPI_COMM_WORLD['machine_db']['remote_launch'][machine_id]
     remote_flags = MPI_COMM_WORLD['machine_db']['remote_flags'][machine_id]

src/pypPython/grid/slurm2hostmap.py
@@ -31,7 +31,7 @@ def slurm2hostmap():
     Date: November 28, 2022
     """
 
-    DEBUG = 0
+    DEBUG = 1
     if DEBUG:
         print('--> Entering slurm2hostmap')
     