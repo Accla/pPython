3cedb92f365329a2dffb1093d34523f9b2edc34e
Chansup Byun <cbyun@ll.mit.edu>
2023-11-22 14:52:45 -0500

    Add additional modules for easy-of-use when writing a pPython code

---
+++ src/pPython/grid.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.append('./grid')
+from check_allowance import *
+from check_mount import *
+from check_runtime import *
+from check_triples import *
+from detect_transport import *
+from exec_shell_cmd import *
+from gen_cpu_list import *
+from get_cpu_info import *
+from grid_abort import *
+from grid_config import *
+from grid_config_init import *
+from grid_resource_policy import *
+from grid_run import *
+from grid_status import *
+from launch_non_triples import *
+from launch_with_triples import *
+from map_procs_to_cores import *
+from set_remote_cc import *
+from slurm2hostmap import *
+from slurm_submit_job import *
+from slurm_write_job_script import *
---
+++ src/pPython/map.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.append('./src/map')
+from Dmap import *
+from inmap import *
+from ones import *
+from rand import *
+from zeros import *
