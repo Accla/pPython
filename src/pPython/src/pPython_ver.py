64c91d947bc343ca8f57fce038d75c45c979b6b6
Byun, Chansup (CH21778) <cbyun@ll.mit.edu>
2025-06-26 13:32:16 -0400

    Created pPython 0.8.13 wheel

--- src/pPython/src/pPython_ver.py
+++ src/pPython/src/pPython_ver.py
@@ -13,7 +13,7 @@ def pPython_ver(default_display = None):
     """
     
     location = pathlib.Path().absolute()
-    version = '0.9.12'
+    version = '0.9.13'
     
     if default_display:
         print('pPython version: %s'%(version))
