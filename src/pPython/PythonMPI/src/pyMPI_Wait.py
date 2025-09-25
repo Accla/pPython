1ffaf465dbf8f83b8b3412ffedabeafe8fc75a49
Chansup Byun <cbyun@ll.mit.edu>
2025-09-25 10:17:02 -0400

    Update wait time loop to be reasonablly long enough before exiting.

--- src/pPython/PythonMPI/src/pyMPI_Wait.py
+++ src/pPython/PythonMPI/src/pyMPI_Wait.py
@@ -1,34 +1,49 @@
 import os
-
 from pyMPI_Sleep import *
 
-def pyMPI_Wait(funcname,filename,logical_state):
+def pyMPI_Wait(funcname,filename,status):
     """
     Wait until the given file reached the desired status
     funcname: calling function
     filename: name of the file to check
-    logical_state: True: -> exits already
-                   False -> not exist yet
+    status: True: -> exits already -> wait until False
+            False -> not exist yet -> wait until True
     """
 
     # How much the pause time gets increased each iteration
-    pause_rate = 0.003
+    pause_rate = 0.009
     # Initial pause time
     pause_init = 0.001
     # max iteration
-    max_iter = 100000
+    max_iter = 900
+    if status:
+        is_done = False
+    else:
+        is_done = status
 
     # Spin on the file until its desired status is reached.
     loop = 0;
     sum = 0;
     pause_time = pause_init
-    while os.path.exists(filename) == logical_state :
+    while not is_done :
         # Sleep statement allows cleaner profiling, but adds latency.
         sum += pause_time
         pyMPI_Sleep(pause_time);
+        if os.path.exists(filename):
+            # file exists
+            if not status:
+                # Wait until file becomes available.
+                is_done = True
+        else: 
+            # file does not exist
+            if status: 
+                # Wait until file gets deleted.
+                is_done = True
+            
         if loop > max_iter:
             print('Loop: %d, total wait time: %f, last pause interval: %f'%(loop,sum,pause_time))
             raise Exception('%s: failed to find the %s file.'%(funcname,filename))
+
         loop = loop + 1
         pause_time += pause_time * pause_rate
     return
