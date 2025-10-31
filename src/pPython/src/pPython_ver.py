f3351f98789434df31cc4114dfb07eefdc010adf
Byun, Chansup (CH21778) <cbyun@ll.mit.edu>
2025-10-31 08:25:35 -0400

    pPython v0.10.1 release candidate (support for partition name in the 3rd argument of pRUN function.)

--- src/pPython/src/pPython_ver.py
+++ src/pPython/src/pPython_ver.py
@@ -13,7 +13,7 @@ def pPython_ver(default_display = None):
     """
     
     location = pathlib.Path().absolute()
-    version = '0.10.0'
+    version = '0.10.1'
     
     if default_display:
         print('pPython version: %s'%(version))
