e7e14053cd615f1946c92b5c72f7d4d915ae81d8
Byun, Chansup (CH21778) <cbyun@ll.mit.edu>
2025-12-16 10:37:13 -0500

    pPython v0.10.2 release

--- src/pPython/src/pPython_ver.py
+++ src/pPython/src/pPython_ver.py
@@ -13,7 +13,7 @@ def pPython_ver(default_display = None):
     """
     
     location = pathlib.Path().absolute()
-    version = '0.10.1'
+    version = '0.10.2'
     
     if default_display:
         print('pPython version: %s'%(version))
